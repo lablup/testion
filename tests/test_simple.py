02a8f8c01b41030f4e6b03b755ba6af93429694e
Joongi Kim <joongi@lablup.com>
2016-10-25 22:04:32 +0900

    Make mocks for all external services as yield fixtures.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -22,23 +22,47 @@ common_data = {
 }
 
 dummy_env = {
-    'GH_USERNAME': 'dummy-user',
-    'GH_TOKEN': 'dummy-password',
 }
 
 
-@pytest.fixture
-def dummy_github():
-    return mock.patch('github3.login')
+@pytest.yield_fixture
+def github_mock():
+    o = mock.patch('github3.login')
+    with o:
+        yield o
 
+@pytest.yield_fixture
+def requests_mock():
+    o = mock.patch('requests.post')
+    with o:
+        yield o
+
+@pytest.yield_fixture
+def aws_mock():
+    o = mock.patch('testion.reporter.mixins.S3LogUploadMixin')
+    with o:
+        yield o
+
+@pytest.yield_fixture
+def env_mock():
+    o = mock.patch.dict('os.environ', {
+        'GH_USERNAME': 'dummy-user',
+        'GH_TOKEN': 'dummy-password',
+        'AWS_ACCESS_KEY_ID': 'MyPreciousKey',
+        'AWS_SECRET_ACCESS_KEY': 'MyPreciousSecret',
+        'AWS_DEFAULT_REGION': 'ap-northeast-2',
+    })
+    with o:
+        yield o
 
 async def test_setup(create_app_and_client):
     app, client = await create_app_and_client()
 
-
-async def test_unittest_mixed(capsys, create_app_and_client, dummy_github):
+async def test_unittest_mixed(capsys, create_app_and_client,
+                              github_mock, requests_mock,
+                              aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=unit-mixed',
             headers={'X-GitHub-Event': 'push'},
@@ -55,9 +79,11 @@ async def test_unittest_mixed(capsys, create_app_and_client, dummy_github):
         assert '(2 / 4) passed' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_unittest_success(capsys, create_app_and_client, dummy_github):
+async def test_unittest_success(capsys, create_app_and_client,
+                                github_mock, requests_mock,
+                                aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=unit-success',
             headers={'X-GitHub-Event': 'push'},
@@ -74,9 +100,11 @@ async def test_unittest_success(capsys, create_app_and_client, dummy_github):
         assert 'All 2 tests OK' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_unittest_failure(capsys, create_app_and_client, dummy_github):
+async def test_unittest_failure(capsys, create_app_and_client,
+                                github_mock, requests_mock,
+                                aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=unit-failure',
             headers={'X-GitHub-Event': 'push'},
@@ -93,9 +121,11 @@ async def test_unittest_failure(capsys, create_app_and_client, dummy_github):
         assert '(0 / 1) passed' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_unittest_errors(capsys, create_app_and_client, dummy_github):
+async def test_unittest_errors(capsys, create_app_and_client,
+                               github_mock, requests_mock,
+                               aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=unit-errors',
             headers={'X-GitHub-Event': 'push'},
@@ -112,9 +142,11 @@ async def test_unittest_errors(capsys, create_app_and_client, dummy_github):
         assert '(0 / 1) passed' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_pytest_mixed(capsys, create_app_and_client, dummy_github):
+async def test_pytest_mixed(capsys, create_app_and_client,
+                            github_mock, requests_mock,
+                            aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=pytest-mixed',
             headers={'X-GitHub-Event': 'push'},
@@ -131,9 +163,11 @@ async def test_pytest_mixed(capsys, create_app_and_client, dummy_github):
         assert '(2 / 4) passed' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_pytest_success(capsys, create_app_and_client, dummy_github):
+async def test_pytest_success(capsys, create_app_and_client,
+                              github_mock, requests_mock,
+                              aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=pytest-success',
             headers={'X-GitHub-Event': 'push'},
@@ -150,9 +184,11 @@ async def test_pytest_success(capsys, create_app_and_client, dummy_github):
         assert 'All 2 tests OK' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_pytest_failure(capsys, create_app_and_client, dummy_github):
+async def test_pytest_failure(capsys, create_app_and_client,
+                              github_mock, requests_mock,
+                              aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=pytest-failure',
             headers={'X-GitHub-Event': 'push'},
@@ -169,9 +205,11 @@ async def test_pytest_failure(capsys, create_app_and_client, dummy_github):
         assert '(0 / 1) passed' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_pytest_errors(capsys, create_app_and_client, dummy_github):
+async def test_pytest_errors(capsys, create_app_and_client,
+                             github_mock, requests_mock,
+                             aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=pytest-errors',
             headers={'X-GitHub-Event': 'push'},
@@ -189,9 +227,11 @@ async def test_pytest_errors(capsys, create_app_and_client, dummy_github):
         assert kwargs['context'] == 'ci/testion/unit-test'
 
 
-async def test_unit_mixed_branch(capsys, create_app_and_client, dummy_github):
+async def test_unit_mixed_branch(capsys, create_app_and_client,
+                                 github_mock, requests_mock,
+                                 aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=unit-mixed-branch',
             headers={'X-GitHub-Event': 'push'},
@@ -208,9 +248,11 @@ async def test_unit_mixed_branch(capsys, create_app_and_client, dummy_github):
         assert '(2 / 4) passed' in kwargs['description']
         assert kwargs['context'] == 'ci/testion/unit-test'
 
-async def test_pytest_mixed_branch(capsys, create_app_and_client, dummy_github):
+async def test_pytest_mixed_branch(capsys, create_app_and_client,
+                                   github_mock, requests_mock,
+                                   aws_mock, env_mock):
     app, client = await create_app_and_client()
-    with capsys.disabled(), mock.patch.dict('os.environ', dummy_env), dummy_github:
+    with capsys.disabled():
         resp = await client.post(
             '/webhook?report=pytest-mixed-branch',
             headers={'X-GitHub-Event': 'push'},
